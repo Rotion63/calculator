5a61029c2507ed468231d8a8d3bd3119521dfe3f
Madhav Prasad Guragain <guragainroshan63@gmail.com>
2021-06-20 11:51:44 +0545

    Making smaller Function and adding comments.

--- script.js
+++ script.js
@@ -10,96 +10,74 @@ let count =0;
 let lengthCount =0;
 let bigNo = 999999999999999;
 
+// asking for first number
 
-    keys.forEach(key => {
+keys.forEach(key => {
+    key.addEventListener('click',(e) => {
 
-        key.addEventListener('click',(e) => {
-            if(e.target.id <= 9 && count == 0){
-
-            firstNumber += e.target.id;
-            display.textContent = firstNumber;
-
-        }else if(e.target.id == "/" || e.target.id == "*" || e.target.id == "+" || e.target.id == "-"){
-
-                    operator = e.target.id;
-                    display.textContent = firstNumber;
-                    count = 1;
-            }
-
-        })
-
-    });
-    keys.forEach(key => {
-
-        key.addEventListener('click',(e) => {
-            if(e.target.id <= 9 && count ==1){
-
-            secondNumber += e.target.id;
-            display.textContent = secondNumber;
-        }else if(e.target.id == "/" || e.target.id == "*" || e.target.id == "+" || e.target.id == "-"){
-
-            operator = e.target.id;
-            display.textContent = firstNumber;
-            count = 1;
-        }else if(e.target.id == "=" && operator == "+" && count == 1){
-            finalNumber = Number(firstNumber) + Number(secondNumber);
-            display.textContent = finalNumber;
-            firstNumber = finalNumber;
-            count = 0;
-            secondNumber = '';
-        }else if(e.target.id == "=" && operator == "-" && count == 1){
-            finalNumber = Number(firstNumber) - Number(secondNumber);
-            display.textContent = finalNumber;
-            firstNumber = finalNumber;
-            count = 0;
-            secondNumber = '';
-        }else if(e.target.id == "=" && operator == "/" && count == 1){
-            finalNumber = Number(firstNumber) / Number(secondNumber);
-            display.textContent = finalNumber;
-            firstNumber = finalNumber;
-            count = 0;
-            secondNumber = '';
-        }else if(e.target.id == "=" && operator == "*" && count == 1){
-            finalNumber = Number(firstNumber) * Number(secondNumber);
-            display.textContent = finalNumber;
-            firstNumber = finalNumber;
-            count = 0;
-            secondNumber = '';
+        if((e.target.id <=9 || e.target.id == '.') && count == '0'){ // if number is presed and count is 0 call firstFunction
+            firstFunction(e.target.id);
+        }else if((e.target.id == "/") || (e.target.id == "*") || (e.target.id == "+") || (e.target.id == "-")){ //if operator is pressed and count is anything call operatorFunction.
+            operatorFunction(e.target.id);
         }
-
-        });
-    });
-
-    // when clear button is pressed it clears all value and display the initial value.
-    //when delete button is pressed it delete the last enter charecter.
-    clearAndDelete.forEach(key => {
-        key.addEventListener('click',(e) => {
-            if(e.target.id == 'clear'){
-                firstNumber = '';
-                secondNumber = '';
-                count = 0;
-                display.textContent = '0';
-            }else if(e.target.id == 'delete'){
-                if(count == 0){
-                    firstNumber = firstNumber.slice(0,-1);
-                    display.textContent = firstNumber;
-                }else if(count == 1){
-                    secondNumber = secondNumber.slice(0,-1);
-                    display.textContent = secondNumber;
-                }
-            }
-        })
-    });
-
-    // when i press the '=' button after the calculation it only shows the answer.
-    keys.forEach(key => {
-            key.addEventListener('click', (e) => {
-                if(e.target.id == '=' && count == 0 && secondNumber == ''){
-
-                        display.textContent = firstNumber;
-                        count = 0;
-                }
-            })
-    });
+    })
+});
 
 
+const firstFunction = (userInput) => { //put userInput it firstNumber
+    firstNumber = userInput;
+}
+
+const operatorFunction = (userInput) => {//put userInput and put it in operator and put count to 1;
+    operator = userInput;
+    count = 1;
+}
+
+keys.forEach(key => {
+    key.addEventListener('click',(e) => {
+
+        if((e.target.id <=9 || e.target.id == '.') && count == '1'){//if number is pressed and count is 1 call secondFunction.
+            secondFunction(e.target.id);
+        }
+    })
+});
+
+const secondFunction = (userInput) => {//when number is pressed it stores in second number. Here count is 1
+    secondNumber = userInput;
+}
+
+keys.forEach(key => {
+    key.addEventListener('click', (e) => {
+
+        if (e.target.id == '='){ //when '=' pressed check the firstNumber and secondNumber and call checkFunction.
+            checkFunction(firstNumber,secondNumber);
+        }
+    })
+});
+
+const checkFunction = (a,b) => {//when firstNumber and secondNumber passed it checks for number is empty or not.
+
+    if(a == ''){// if a=firstNumber is empty. put finalNumber = 0. it dont care about the operator is empty or not.
+        finalNumber = 0;
+    }else if(a != '' && b == '')//if a=firstNumber is non empty and b=secondNumber is empty. put final Number = a;
+    {
+        finalNumber = a;
+    }else if(a != '' && b != '' && operator != ''){//ig both a and b are non empty then call calculationFunction
+        
+        calculationFunction(a,b);
+    }
+}
+
+const calculationFunction = (valueA,valueB) => {//when both firstNumber is non empty and operator is present it do the calculation.
+
+    if(operator == '+'){
+        finalNumber = Number(valueA) + Number(valueB);
+    }else if(operator == '-'){
+        finalNumber = Number(valueA) - Number(valueB);
+    }else if(operator == '*'){
+        finalNumber = Number(valueA) * Number(valueB);
+    }else if(operator == '/'){
+        finalNumber = Number(valueA) / Number(valueB);
+    }
+
+}
--- study.js
+++ study.js
@@ -1,137 +1,83 @@
-keys.forEach(key => {
+const display = document.querySelector('#display');
+const keys = document.querySelectorAll('.keySection');
+const clearAndDelete = document.querySelectorAll('.clearAndDelete');
+//
+let firstNumber = '';
+let secondNumber = '';
+let finalNumber = 0;
+let operator = '';
+let count =0;
+let lengthCount =0;
+let bigNo = 999999999999999;
 
-    key.addEventListener('click',(e) => {
-        if((e.target.id <= 9 || e.target.id == '.') && count == 0){
-
-            if(lengthCount < 15){
-
-        firstNumber += e.target.id;
-        display.textContent = firstNumber;
-
-        lengthCount++;
-
-        }
-
-    }
-
-    })
-
-});
+// asking for first number
 
 keys.forEach(key => {
-
     key.addEventListener('click',(e) => {
-if((e.target.id == "/" && secondNumber == '' && firstNumber != '') || (e.target.id == "*" && secondNumber == '' && firstNumber != '') || (e.target.id == "+" && secondNumber == '' && firstNumber != '') || (e.target.id == "-" && secondNumber == '' && firstNumber != '')){
 
-    operator = e.target.id;
-    display.textContent = firstNumber;
-    count = 1;
-    lengthCount = 0;
-}else  if((e.target.id == "/" && secondNumber != '' && firstNumber != '') || (e.target.id == "*" && secondNumber != '' && firstNumber != '') || (e.target.id == "+" && secondNumber != '' && firstNumber != '') || (e.target.id == "-" && secondNumber != '' && firstNumber != '')){
-
-operator = e.target.id;
-display.textContent = calculation(firstNumber,secondNumber);
-firstNumber = calculation(firstNumber,secondNumber);
-secondNumber = '';
-count = 1;
-lengthCount = 0;
-}
-})
-
-});
-
-keys.forEach(key => {
-
-    key.addEventListener('click',(e) => {
-        if((e.target.id <= 9 || e.target.id == '.') && count ==1 && firstNumber != ''){
-
-            if(lengthCount < 15){
-
-        secondNumber += e.target.id;
-        display.textContent = secondNumber;
-        lengthCount++
-
-        }
-}
-});
-});
-
-    
-keys.forEach(key => {
-key.addEventListener('click', (e) => {
-
-    if(e.target.id == '=' && count == 1 && firstNumber != '' && secondNumber != ''){
-
-        display.textContent = calculation(firstNumber,secondNumber);
-        firstNumber = calculation(firstNumber,secondNumber);
-        secondNumber = '';
-        count = 1;
-        lengthCount = 0;
-    }
-});
-});
-
-// when clear button is pressed it clears all value and display the initial value.
-//when delete button is pressed it delete the last enter charecter.
-clearAndDelete.forEach(key => {
-    key.addEventListener('click',(e) => {
-        if(e.target.id == 'clear'){
-            firstNumber = '';
-            secondNumber = '';
-            finalNumber = 0
-            count = 0;
-            display.textContent = '0';
-            lengthCount = 0;
-        }else if(e.target.id == 'delete'){
-            if(count == 0){
-                firstNumber = (firstNumber.toString()).slice(0,-1);
-                display.textContent = firstNumber;
-                lengthCount--;
-            }else if(count == 1){
-                secondNumber = secondNumber.slice(0,-1);
-                display.textContent = secondNumber;
-                lengthCount--
-            }
+        if((e.target.id <=9 || e.target.id == '.') && count == '0'){ // if number is presed and count is 0 call firstFunction
+            firstFunction(e.target.id);
+        }else if((e.target.id == "/") || (e.target.id == "*") || (e.target.id == "+") || (e.target.id == "-")){ //if operator is pressed and count is anything call operatorFunction.
+            operatorFunction(e.target.id);
         }
     })
 });
 
 
-// when i press the '=' button after the calculation it only shows the answer.
-
-
-const calculation = function(a,b){
-if(operator == "+"){
-    finalNumber =Math.round((Number(a) + Number(b))*100000)/100000;
-    display.textContent =Math.round(finalNumber*1000)/1000;
-
-    count = 1;
-    firstNumber = finalNumber;
-    secondNumber = '';
-    return finalNumber;
-
-}else if(operator == "-" && count == 1){
-    finalNumber =Math.round((Number(a) - Number(b))*100000)/100000;
-    display.textContent =Math.round(finalNumber*1000)/1000;
-    count = 1;
-    firstNumber = finalNumber;
-    secondNumber = '';
-    return finalNumber;
-
-}else if(operator == "/" && count == 1){
-    finalNumber =Math.round((Number(a) / Number(b))*100000)/100000;
-    display.textContent =Math.round(finalNumber*1000)/1000;
-    count = 1;
-    firstNumber = finalNumber;
-    secondNumber = '';
-    return finalNumber;
-
-}else if(operator == "*" && count == 1){
-    finalNumber =Math.round((Number(a) * Number(b))*100000)/100000;
-    display.textContent =Math.round(finalNumber*1000)/1000;
-    count = 1;
-    firstNumber = finalNumber;
-    secondNumber = '';
-    return finalNumber;
+const firstFunction = (userInput) => { //put userInput it firstNumber
+    firstNumber = userInput;
 }
+
+const operatorFunction = (userInput) => {//put userInput and put it in operator and put count to 1;
+    operator = userInput;
+    count = 1;
+}
+
+keys.forEach(key => {
+    key.addEventListener('click',(e) => {
+
+        if((e.target.id <=9 || e.target.id == '.') && count == '1'){//if number is pressed and count is 1 call secondFunction.
+            secondFunction(e.target.id);
+        }
+    })
+});
+
+const secondFunction = (userInput) => {//when number is pressed it stores in second number. Here count is 1
+    secondNumber = userInput;
+}
+
+keys.forEach(key => {
+    key.addEventListener('click', (e) => {
+
+        if (e.target.id == '='){ //when '=' pressed check the firstNumber and secondNumber and call checkFunction.
+            checkFunction(firstNumber,secondNumber);
+        }
+    })
+});
+
+const checkFunction = (a,b) => {//when firstNumber and secondNumber passed it checks for number is empty or not.
+
+    if(a == ''){// if a=firstNumber is empty. put finalNumber = 0. it dont care about the operator is empty or not.
+        finalNumber = 0;
+    }else if(a != '' && b == '')//if a=firstNumber is non empty and b=secondNumber is empty. put final Number = a;
+    {
+        finalNumber = a;
+    }else if(a != '' && b != '' && operator != ''){//ig both a and b are non empty then call calculationFunction
+        
+        calculationFunction(a,b);
+    }
+}
+
+const calculationFunction = (valueA,valueB) => {//when both firstNumber is non empty and operator is present it do the calculation.
+
+    if(operator == '+'){
+        finalNumber = Number(valueA) + Number(valueB);
+    }else if(operator == '-'){
+        finalNumber = Number(valueA) - Number(valueB);
+    }else if(operator == '*'){
+        finalNumber = Number(valueA) * Number(valueB);
+    }else if(operator == '/'){
+        finalNumber = Number(valueA) / Number(valueB);
+    }
+
 }
