ac394fd716d373853ec89c4bef54c4f0ec8a5edc
Madhav Prasad Guragain <guragainroshan63@gmail.com>
2021-06-20 13:34:26 +0545

    Version 0.5== Fully functional but chareters length is not fixed

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ keys.forEach(key => {
     key.addEventListener('click',(e) => {
 
         if((e.target.id <=9 || e.target.id == '.') && count == '0'){ // if number is presed and count is 0 call firstFunction
+        
             firstFunction(e.target.id);
         }else if((e.target.id == "/") || (e.target.id == "*") || (e.target.id == "+") || (e.target.id == "-")){ //if operator is pressed and count is anything call operatorFunction.
             operatorFunction(e.target.id);
@@ -25,12 +26,22 @@ keys.forEach(key => {
 
 
 const firstFunction = (userInput) => { //put userInput it firstNumber
-    firstNumber = userInput;
+    firstNumber += userInput;
+    display.textContent = firstNumber;
 }
 
 const operatorFunction = (userInput) => {//put userInput and put it in operator and put count to 1;
+    
+    if(firstNumber != '' && secondNumber == ''){
     operator = userInput;
+    display.textContent = firstNumber;
     count = 1;
+
+    }else if(firstNumber != '' && secondNumber != ''){
+        checkFunction(firstNumber,secondNumber);
+        operator = userInput;
+
+    }
 }
 
 keys.forEach(key => {
@@ -43,7 +54,8 @@ keys.forEach(key => {
 });
 
 const secondFunction = (userInput) => {//when number is pressed it stores in second number. Here count is 1
-    secondNumber = userInput;
+    secondNumber += userInput;
+    display.textContent = secondNumber;
 }
 
 keys.forEach(key => {
@@ -51,6 +63,8 @@ keys.forEach(key => {
 
         if (e.target.id == '='){ //when '=' pressed check the firstNumber and secondNumber and call checkFunction.
             checkFunction(firstNumber,secondNumber);
+            operator ='';
+            count = 0;
         }
     })
 });
@@ -59,9 +73,11 @@ const checkFunction = (a,b) => {//when firstNumber and secondNumber passed it ch
 
     if(a == ''){// if a=firstNumber is empty. put finalNumber = 0. it dont care about the operator is empty or not.
         finalNumber = 0;
+        display.textContent = finalNumber;
     }else if(a != '' && b == '')//if a=firstNumber is non empty and b=secondNumber is empty. put final Number = a;
     {
         finalNumber = a;
+        display.textContent = finalNumber;
     }else if(a != '' && b != '' && operator != ''){//ig both a and b are non empty then call calculationFunction
         
         calculationFunction(a,b);
@@ -72,12 +88,48 @@ const calculationFunction = (valueA,valueB) => {//when both firstNumber is non e
 
     if(operator == '+'){
         finalNumber = Number(valueA) + Number(valueB);
+        display.textContent = finalNumber;
+        firstNumber = finalNumber;
+        secondNumber ='';
+
     }else if(operator == '-'){
         finalNumber = Number(valueA) - Number(valueB);
+        display.textContent = finalNumber;
+        firstNumber = finalNumber;
+        secondNumber ='';
+
     }else if(operator == '*'){
         finalNumber = Number(valueA) * Number(valueB);
+        display.textContent = finalNumber;
+        firstNumber = finalNumber;
+        secondNumber ='';
+
     }else if(operator == '/'){
         finalNumber = Number(valueA) / Number(valueB);
+        display.textContent = finalNumber;
+        firstNumber = finalNumber;
+        secondNumber ='';
     }
 
 }
+
+    // when clear button is pressed it clears all value and display the initial value.
+    //when delete button is pressed it delete the last enter charecter.
+    clearAndDelete.forEach(key => {
+        key.addEventListener('click',(e) => {
+            if(e.target.id == 'clear'){
+                firstNumber = '';
+                secondNumber = '';
+                count = 0;
+                display.textContent = 0;
+            }else if(e.target.id == 'delete'){
+                if(count == 0){
+                    firstNumber = firstNumber.slice(0,-1);
+                    display.textContent = firstNumber;
+                }else if(count == 1){
+                    secondNumber = secondNumber.slice(0,-1);
+                    display.textContent = secondNumber;
+                }
+            }
+        })
+    });
